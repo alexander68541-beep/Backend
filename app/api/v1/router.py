a6545b18ed4bdd8c5853c2a94e7f9255bf189b95
alexander68541-beep <alexander68541@gmail.com>
2026-09-24 15:48:02 +0600

    Delete app directory

--- app/api/v1/router.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.v1.routes import account, health, portfolio, username
-
-api_router = APIRouter()
-api_router.include_router(health.router)
-api_router.include_router(account.router)
-api_router.include_router(portfolio.router)
-api_router.include_router(username.router)
